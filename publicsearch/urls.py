6f9cf3c481f8335181f1a19cb039a8368affa812
JulianJaffe <julianjaffe@berkeley.edu>
2014-04-08 12:01:13 -0700

    Blocked out new config file and template changes, made URLS case-insensitive

--- publicsearch/urls.py
+++ publicsearch/urls.py
@@ -4,9 +4,9 @@ from django.conf.urls import patterns, url
 from publicsearch import views
 
 urlpatterns = patterns('',
-                       url(r'^search/$', views.publicsearch, name='publicSearch'),
-                       url(r'^results/$', views.retrieveResults, name='retrieveResults'),
-                       url(r'^bmapper/$', views.bmapper, name='bmapper'),
-                       url(r'^csv/$', views.csv, name='csv'),
-                       url(r'^gmapper/$', views.gmapper, name='gmapper'),
+                       url(r'^(?i)search/?$', views.publicsearch, name='publicSearch'),
+                       url(r'^(?i)results/?$', views.retrieveResults, name='retrieveResults'),
+                       url(r'^(?i)bmapper/?$', views.bmapper, name='bmapper'),
+                       url(r'^(?i)csv/?$', views.csv, name='csv'),
+                       url(r'^(?i)gmapper/?$', views.gmapper, name='gmapper'),
                        )
